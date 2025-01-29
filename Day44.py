3ebd96a91630cad00b19a8bfc96c92b9bbf2bf5e
Ahmed Raza <81850130+AhmedRaza11-IT@users.noreply.github.com>
2025-01-29 16:10:18 +0500

    Day44 done

--- Day44.py
+++ Day44.py
@@ -1,2 +1,19 @@
-# import statement
-import random
+# # import statement
+# # import math
+# import math as m
+# import random
+# from math import sqrt,pi
+#
+# # import pandas as pd
+# # pd.read_csv()
+#
+# # print(math.floor(5.255252))
+# print(m.floor(5.255252))
+# result=sqrt(45454)*pi
+# print(result)
+# from Day44_1 import Welcome,ahmed
+from Day44_1 import *
+import math
+print(dir(math))
+Welcome()
+print(ahmed)
---
+++ Day44_1.py
@@ -0,0 +1,5 @@
+def Welcome():
+    print("How are you")
+
+
+ahmed = "Backend developer"
